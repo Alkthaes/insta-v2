d49307756124ef06af9e48daac137097a00649e9
Anthony Bigotte <bigotte.anthony@gmail.com>
2023-03-23 14:13:32 +0100

    get posts from the db

--- components/Posts.jsx
+++ components/Posts.jsx
@@ -1,36 +1,29 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import Post from './Post';
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { db } from '../firebase';
 
 export default function Posts() {
-  const dummyPosts = [
-    {
-      id: 1,
-      username: 'johnny_silverhand',
-      userImg:
-        'https://cdn-cf.ginx.tv/respawn-cdn/mV6-7qivPHPLOY0xShEBDbIowWshVOf9ig2f_JyLa60/fill/1280/0/no/1/aHR0cHM6Ly93d3cuZ2lueC50di91cGxvYWRzL3Bhc3RlZF9pbWFnZV8wLTEyNS5wbmc.webp',
-      img: 'https://cdn-cf.ginx.tv/respawn-cdn/mV6-7qivPHPLOY0xShEBDbIowWshVOf9ig2f_JyLa60/fill/1280/0/no/1/aHR0cHM6Ly93d3cuZ2lueC50di91cGxvYWRzL3Bhc3RlZF9pbWFnZV8wLTEyNS5wbmc.webp',
-      caption: 'Wake up Samurai, we have a city to burn.',
-    },
-    {
-      id: 2,
-      username: 'johnny_silverhand',
-      userImg:
-        'https://cdn-cf.ginx.tv/respawn-cdn/mV6-7qivPHPLOY0xShEBDbIowWshVOf9ig2f_JyLa60/fill/1280/0/no/1/aHR0cHM6Ly93d3cuZ2lueC50di91cGxvYWRzL3Bhc3RlZF9pbWFnZV8wLTEyNS5wbmc.webp',
-      img: 'https://cdn-cf.ginx.tv/respawn-cdn/mV6-7qivPHPLOY0xShEBDbIowWshVOf9ig2f_JyLa60/fill/1280/0/no/1/aHR0cHM6Ly93d3cuZ2lueC50di91cGxvYWRzL3Bhc3RlZF9pbWFnZV8wLTEyNS5wbmc.webp',
-      caption: 'Wake up Samurai, we have a city to burn.',
-    },
-  ];
-
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      query(collection(db, 'posts'), orderBy('timestamp', 'desc')),
+      (snapshot) => {
+        setPosts(snapshot.docs);
+      }
+    );
+    return unsubscribe;
+  }, [db]);
   return (
     <div>
-      {dummyPosts.map((post) => (
+      {posts.map((post) => (
         <Post
           key={post.id}
           id={post.id}
-          username={post.username}
-          userImg={post.userImg}
-          img={post.img}
-          caption={post.caption}
+          username={post.data().username}
+          userImg={post.data().profileImg}
+          img={post.data().image}
+          caption={post.data().caption}
         />
       ))}
     </div>
